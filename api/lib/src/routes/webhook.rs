24d2ec1c355ba6155383c8bcffbc82fecf347ffb
Yuki Yoshimine <yuki07yuki@gmail.com>
2024-05-29 18:09:20 +0900

    feat: add item and line_item to the database

--- api/lib/src/routes/webhook.rs
+++ api/lib/src/routes/webhook.rs
@@ -18,7 +18,7 @@ pub async fn webhook(
     println!("{:#?}", invoice);
 
     // insert the invoice into the database
-    let invoice = sqlx::query(
+    sqlx::query(
         "INSERT INTO invoice (created_by_name,
                                 created_date,
                                 customer_id,
@@ -28,15 +28,14 @@ pub async fn webhook(
                                 invoice_number,
                                 status,
                                 total)
-            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
-            RETURNING invoice_id",
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)"
     )
     .bind(invoice.created_by_name)
     .bind(invoice.created_date)
     .bind(invoice.customer_id)
     .bind(invoice.customer_name)
     .bind(invoice.date)
-    .bind(invoice.invoice_id)
+    .bind(&invoice.invoice_id)
     .bind(invoice.invoice_number)
     .bind(invoice.status)
     .bind(invoice.total)
@@ -44,8 +43,45 @@ pub async fn webhook(
     .await
     .map_err(Error::from)?;
 
-    if invoice.rows_affected() != 1 {
-        return Err(Error::from("Failed to insert invoice"));
+    let line_items = invoice.line_items;
+    // check if line_item.item_id is already in Item table
+    for line_item in line_items {
+        let res = sqlx::query("SELECT item_id FROM item WHERE item_id = $1")
+            .bind(&line_item.item_id)
+            .fetch_optional(&state.pool)
+            .await
+            .map_err(Error::from)?;
+
+        if res.is_none() {
+            // insert the item into the item table
+            sqlx::query(
+                "INSERT INTO item (item_id, name, purchase_rate, rate)
+                    VALUES ($1, $2, $3, $4)",
+            )
+            .bind(&line_item.item_id)
+            .bind(line_item.name)
+            .bind(line_item.purchase_rate)
+            .bind(line_item.rate)
+            .execute(&state.pool)
+            .await
+            .map_err(Error::from)?;
+        }
+
+        // insert line_item into the line_item table
+        sqlx::query(
+            "INSERT INTO LineItem (line_item_id, invoice_id, item_id, item_total, quantity, rate)
+                VALUES ($1, $2, $3, $4, $5, $6)",
+        )
+        .bind(&line_item.line_item_id)
+        .bind(&invoice.invoice_id)
+        .bind(line_item.item_id)
+        .bind(line_item.item_total)
+        .bind(line_item.quantity)
+        .bind(line_item.rate)
+        .execute(&state.pool)
+        .await
+        .map_err(Error::from)?;
+
     }
 
     Ok((StatusCode::OK, "Success!"))
